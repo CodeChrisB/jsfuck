53877fa1ba4f77676d14ea51517896b0d0c2d38d
CodeChrisB <c.buchberger01@gmail.com>
2022-08-15 19:01:55 +0200

    jsfuck

---
+++ jsfuck.js
@@ -0,0 +1,82 @@
+
+const zero = '+[]';
+const one = '+!![]';
+
+const number = n => {
+  if (n === 0) return zero;
+  return Array.from({length: n}, () => one).join(' + ');
+}
+
+
+
+
+const fromString = s =>s.split('').map(x => {
+  if (!(x in map)) {
+    const charCode = x.charCodeAt(0);
+    return `([]+[])[${fromString('constructor')}][${fromString('fromCharCode')}](${number(charCode)})`;
+  }
+  return map[x];
+}).join('+');
+let  map = {};
+map.a = `(+{}+[])[${number(1)}]`;
+map.b = `({}+[])[${number(2)}]`;
+map.o = `({}+[])[${number(1)}]`;
+map.e = `({}+[])[${number(4)}]`;
+map.c = `({}+[])[${number(5)}]`;
+map.t = `({}+[])[${number(6)}]`;
+map[' '] = `({}+[])[${number(7)}]`;
+map.f = `(![]+[])[${number(0)}]`;
+map.s = `(![]+[])[${number(3)}]`;
+map.r = `(!![]+[])[${number(1)}]`;
+map.u = `(!![]+[])[${number(2)}]`;
+map.i = `((+!![]/+[])+[])[${number(3)}]`;
+map.n = `((+!![]/+[])+[])[${number(4)}]`;
+map.S = `([]+([]+[])[${fromString('constructor')}])[${number(9)}]`;
+map.g = `([]+([]+[])[${fromString('constructor')}])[${number(14)}]`;
+map.p = `([]+(/-/)[${fromString('constructor')}])[${number(14)}]`;
+map['\\'] = `(/\\\\/+[])[${number(1)}]`;
+map.d = `(${number(13)})[${fromString('toString')}](${number(14)})`;
+map.h = `(${number(17)})[${fromString('toString')}](${number(18)})`;
+map.m = `(${number(22)})[${fromString('toString')}](${number(23)})`;
+map.C = `((()=>{})[${fromString('constructor')}](${fromString('return escape')})()(${map['\\']}))[${number(2)}]`;
+
+const compile = code => `(()=>{})[${fromString('constructor')}](${fromString(code)})()`;
+
+
+
+function transpile(){
+  let js = document.getElementById("jsCode").value
+  let fuck = document.getElementById("jsFuck")
+
+  fuck.innerHTML = compile(js)
+
+}
+
+function callFuck(){
+  let fuck = document.getElementById("jsFuck").value
+  eval(fuck)
+}
+
+function load(){
+  document.getElementById("jsCode").value = `function isPrime(num) {
+    for ( var i = 2; i < num; i++ ) {
+        if ( num % i === 0 ) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function display(n) {
+    let list = []
+    var arr = [2];
+    for ( var i = 3; i < n; i+=2 ) {
+        if ( isPrime(i) ) {
+            arr.push(i);
+        }
+    }
+    alert(arr); // use arr result on your own
+}
+
+display(100);`
+}
